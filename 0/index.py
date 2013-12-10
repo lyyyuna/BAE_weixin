c6f5999dc1ceebeb8f5dc41463ffcd2124d48638
lyyyuna <lyyyuna@gmail.com>
2013-12-10 15:18:11 +0800

    t

--- 0/index.py
+++ 0/index.py
@@ -1,13 +1,33 @@
 #-*- coding:utf-8 -*-
+#! /usr/bin/env python
+# coding=utf-8
+__author__ = 'lyyyuna'
+from bottle import *
+import hashlib
+import xml.etree.ElementTree as ET
+import urllib2
+# import requests
+import json
+ 
+@get("/")
+def checkSignature():
+    token = "*******"  
+    signature = request.GET.get('signature', None)
+    timestamp = request.GET.get('timestamp', None)
+    nonce = request.GET.get('nonce', None)
+    echostr = request.GET.get('echostr', None)
+    tmpList = [token, timestamp, nonce]
+    tmpList.sort()
+    tmpstr = "%s%s%s" % tuple(tmpList)
+    hashstr = hashlib.sha1(tmpstr).hexdigest()
+    if hashstr == signature:
+        return echostr
+    else:
+        return None
+ 
 
-from flask import Flask, g, request 
- 
-app = Flask(__name__)
-app.debug = True
- 
-@app.route('/')
-def hello():
-    return "Hello, world! - Flask\n"
- 
+# Mod WSGI launch
 from bae.core.wsgi import WSGIApplication
+
+app = default_app()
 application = WSGIApplication(app)
